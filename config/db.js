d332b0f3b1e32cca325123cb615eb281da09113e
Asmit-Rai <er.asmitrai@gmail.com>
2024-11-16 02:51:27 +0530

    Mogodb file changes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,35 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://d-transaction-app:mongodb@cluster0.cgkt3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((err) => {
-    console.error('MongoDB connection error:', err.message);
-  });
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-module.exports = mongoose;
+// Use environment variables for sensitive information
+require('dotenv').config();
+
+const uri = `mongodb+srv://d-transaction-app:${process.env.DB_PASSWORD}@cluster0.cgkt3.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Connect to the MongoDB server and export the client for use in your application
+const connectToMongoDB = async () => {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Ping the database to confirm a successful connection
+    await client.db().command({ ping: 1 });
+    console.log("Connected successfully to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // Exit the process if unable to connect
+  }
+};
+
+// Call the function to initiate the connection
+connectToMongoDB();
+
+module.exports = client;
