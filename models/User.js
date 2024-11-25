52a37edbc4daf31704957d06e977cb7ae30057d7
Asmit-Rai <er.asmitrai@gmail.com>
2024-11-26 04:21:16 +0530

    Payment Status

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({ 
+const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     groups: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Group' }],
     createdAt: { type: Date, default: Date.now },
-    name: { type: String },
 });
 
 module.exports = mongoose.model('User', userSchema);
