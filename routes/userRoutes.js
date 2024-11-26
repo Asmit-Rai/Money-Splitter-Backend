ca60e9b0b9b5a9d42e5094592b95bc68635a1505
Asmit-Rai <er.asmitrai@gmail.com>
2024-11-26 16:59:30 +0530

    File added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post('/add-users', userController.addUser);
 router.post('/get-user-id', userController.getUserId)
+router.get('/user/:id', userController.getUserById);
 
 module.exports = router;
