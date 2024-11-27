81acc6022524688db7906921f5c45cba1ad749c4
Asmit-Rai <er.asmitrai@gmail.com>
2024-11-27 05:41:48 +0530

    major change

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,3 +45,20 @@ exports.getUserId = async (req, res) => {
     return res.status(500).json({ error: 'Failed to fetch user ID', details: error.message });
   }
 };
+
+exports.getUserById = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Error fetching user:', error.message);
+    res.status(500).json({ error: 'Failed to fetch user', details: error.message });
+  }
+};
