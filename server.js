be1f8417973aba88db9de5711caeb7cf53ea7616
Asmit Rai <er.asmitrai@gmail.com>
2025-04-26 12:15:17 +0530

    Refactor server.js for improved code organization and clarity; add comments for middleware and routes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -6,21 +5,21 @@ const mongoose = require('./config/db');
 const groupRoutes = require('./routes/groupRoutes'); 
 const userRoutes = require('./routes/userRoutes'); 
 const expenseRoutes = require('./routes/expenseRoutes');
-
 require('dotenv').config(); 
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
+// Routes
 app.use('/api/groups', groupRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-
+// Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
