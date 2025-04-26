3775d4a0c9a411c065b0967b2e028c7dba997c9a
Asmit Rai <er.asmitrai@gmail.com>
2025-04-26 21:43:21 +0530

    Refactor db.js to improve code formatting and maintainability; streamline MongoDB connection logic

--- server.js
+++ server.js
@@ -10,16 +10,13 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
 app.use('/api/groups', groupRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
 
-// Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
