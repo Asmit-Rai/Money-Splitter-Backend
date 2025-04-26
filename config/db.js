98a8062d7eb896d7cfb20e2a8659cac2dec01551
Asmit Rai <er.asmitrai@gmail.com>
2025-04-27 00:26:31 +0530

    Add Stripe integration for payment processing; update environment variables and refactor expense routes

--- config/db.js
+++ config/db.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
 
-const mongoURI ="mongodb+srv://d-transaction-app:mongodb@cluster0.cgkt3.mongodb.net/transaction-app?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI =
+  "mongodb+srv://d-transaction-app:mongodb@cluster0.cgkt3.mongodb.net/transaction-app?retryWrites=true&w=majority&appName=Cluster0";
 
-  
-  const connectToMongoDB = async () => {
-    try {
-      await mongoose.connect(mongoURI, {
-        serverSelectionTimeoutMS: 5000, 
-      });
-      console.log("MongoDB connected successfully!");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error.message);
-      process.exit(1); 
-    }
-  };
-  
-  connectToMongoDB();
-  
-  module.exports = mongoose;
-  
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+    });
+    console.log("MongoDB connected successfully!");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process if unable to connect
+  }
+};
+
+connectToMongoDB();
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -10,13 +10,49 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 
+// Routes
 app.use('/api/groups', groupRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/expenses', expenseRoutes);
 
+<<<<<<< HEAD
+const stripe = require('stripe')('sk_test_51QIsU3EbklFJ2mKnnrzbqAr4bY46VehpMZz1PTGJYnyht0ipOwxX4hWMXCaCMvsVpr5nhBw5kC3plHCKSX2Vobh300gBqd7bPM');
+
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    { customer: customer.id },
+    { apiVersion: '2024-10-28.acacia' }
+  );
+  const money = req.body.billAmount * 100; // Convert to smallest currency unit
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: money,
+    currency: 'eur',
+    customer: customer.id,
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntentId: paymentIntent.id,
+    paymentIntentClientSecret: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: 'pk_test_51QIsU3EbklFJ2mKnHm8ptUIow6AueGIYorOvFKJRAzzfZk7AHYCqAqGMZ2tx1vlUe8nDCltXQUIQPIA8mcLBLdt100oydNPXKQ'
+  });
+});
+
+
+// Start the server
+=======
+// Start server
+>>>>>>> 10e87b217b7ff1bf842b2f1316f7bc9ac2c1b6b3
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
